e5d3c1c4de14c89a857e6faae64ca1258ae027c2
FerdinandChagas <ferdinandsc@yahoo.com.br>
2022-09-08 20:28:17 -0300

    Autenticação Finalizada

--- reclame/urls.py
+++ reclame/urls.py
@@ -5,7 +5,10 @@ app_name = 'reclame'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('cad/', views.cadUsuario, name='cad'),
+    path('cad/', views.cadView, name='cad'),
+    path('cad_user/', views.cadUsuario, name='cad_user'),
     path('home/', views.index, name='index'),
-    path('autentica/', views.autentica, name='autentica')
+    path('autentica/', views.autentica, name='autentica'),
+    path('logout/', views.logout_view, name='logout')
+    
 ]
--- reclame/views.py
+++ reclame/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpRequest,HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login, logout
 
 from .models import Usuario
 
@@ -12,19 +12,29 @@ def index(request):
     return render(request, 'reclame/index.html')
 
 def cadUsuario(request):
-    u = User.objects.create_user(request.POST['login'],request.POST['email'], request.POST['password'])
-    u.set_password = request.POST['password']
+    u = User.objects.create_user(username=request.POST['login'],email=request.POST['email'], password=request.POST['password'])
+    #u.email = request.POST['email']
+    #u.set_password = request.POST['password']
     u.first_name = request.POST['first_name']
-    
-    
+    u.is_staff = True
     u.save()
     
+    return render(request,'reclame/index.html', { 'feedback': 'Cadastro realizado com sucesso!'})
+
+def cadView(request):
     return render(request,'reclame/cadusuario.html')
 
 def autentica(request):
-    user = authenticate(login=request.POST['login'],password=request.POST['password'])
+    user = authenticate(request, username=request.POST['login'],password=request.POST['password'])
+    
     if user is not None:
+        login(request, user)
         return render(request, 'reclame/home.html', { 'user': user })
     else:
         return render(request, 'reclame/index.html', { 'feedback': 'Falha ao tentar relizar login' })
-        
+    
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'reclame/index.html')
+    
